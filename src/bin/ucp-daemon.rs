46895d5cae71938fdd270f3875f7c42b9d9551c3
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2022-06-17 20:36:07 -0700

    Removed daemonize; application dies correctly

--- src/bin/ucp-daemon.rs
+++ src/bin/ucp-daemon.rs
@@ -1,24 +1,22 @@
 extern crate daemonize;
 extern crate x11_clipboard;
 
-use std::fs::File;
-use daemonize::Daemonize;
+//use std::fs::File;
+//use daemonize::Daemonize;
 use x11_clipboard::Clipboard;
 
 use std::collections::HashMap;
 use structopt::StructOpt;
-use std::path::Path;
+use single_instance::SingleInstance;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, StructOpt, Serialize, Deserialize)]
+#[derive(Clone, Debug, StructOpt, Serialize, Deserialize)]
 struct Args {
     #[structopt(short, long, default_value = "http://localhost")]
     url: String,
     #[structopt(short, long, default_value = "8000")]
     port: String,
-    #[structopt(short, long)]
-    kill: bool,
     #[structopt(short, long, default_value = "")]
     hostname: String,
     #[structopt(long, default_value = "")]
@@ -26,129 +24,80 @@ struct Args {
 }
 
 fn main() {
-    let mut opt = Args::from_args();
-    sanitize_input(&mut opt);
 
-    let stdout = File::create("daemon.out").unwrap();
-    let stderr = File::create("error.log").unwrap();
+    let my_instance = SingleInstance::new("ucp-daemon").unwrap();
 
-    if opt.kill {
-        let status = match std::fs::read_to_string("daemon.pid") {
-            Ok(contents) => std::process::Command::new("kill").arg(contents).status(),
-            Err(e) => panic!("Error opening PID file: {:?}", e),
-        };
+    if my_instance.is_single() {
 
-        println!("{:?}", status);
-
-        let d = std::fs::read_to_string("arguments.json").expect("Unable to read arguments file.");
-
-        let old_opt: Args = serde_json::from_str(&d).unwrap();
-
-        match kill_clipboard(&old_opt) {
-            Ok(_) => (),
-            Err(e) => eprintln!("{}", e),
-        };
-
-        if Path::new("arguments.json").exists() {
-            std::fs::remove_file("arguments.json").expect("Unable to remove file.");
+        let mut opt = Args::from_args();
+        sanitize_input(&mut opt);
+    
+        if opt.hostname.trim().is_empty() {
+            let hostname = hostname::get().expect("Failed to retrieve hostname.");
+            opt.hostname = hostname.into_string().unwrap();
         }
 
-        if Path::new("daemon.pid").exists() {
-            std::fs::remove_file("daemon.pid").expect("Unable to remove file.");
-        }
-        
-        std::process::exit(1)
-    }
+        let closure_opt = opt.clone();
+        let client = reqwest::blocking::Client::new();
 
-    if opt.hostname.trim().is_empty() {
-        let hostname = hostname::get().expect("Failed to retrieve hostname.");
-        opt.hostname = hostname.into_string().unwrap();
-
-        let arguments_json =
-            serde_json::to_string(&opt).expect("Failed to convert hostname to JSON.");
-
-        // hostname command moves us into /, so we need to return to the executable's directory
-        let mut curr_dir = std::env::current_exe().unwrap();
-
-        curr_dir.pop();
-        std::env::set_current_dir(&curr_dir).expect("Unable to change to new directory.");
-
-        match std::fs::write("arguments.json", arguments_json) {
-            Ok(_) => (),
-            Err(e) => panic!("{}", e),
-        };
-    }
-
-    // don't open a new instance unless PID file is removed
-
-    if !Path::new("daemon.pid").exists() {
-
-        let daemon = Daemonize::new()
-            .stdout(stdout)
-            .stderr(stderr)
-            .pid_file("daemon.pid");
-
-        match daemon.start() {
-            Ok(_) => println!("Started daemon!"),
-            Err(e) => {
-                eprintln!("Error starting daemon: {}", e);
-                die()
+        let _ = ctrlc::set_handler(move || {
+            match kill_clipboard(&closure_opt) {
+                Ok(_) => (),
+                Err(e) => {
+                    eprintln!("{}", e);            
+                }
             }
-        };
-
-        let client = reqwest::blocking::Client::new();    
+            std::process::exit(-1);        
+        });
 
         match create_clipboard(&opt, &client) {
             Ok(_) => (),
             Err(e) => {
                 eprintln!("{}", e);
-                die()
-            },
-        };
-
-    
+                std::process::exit(-1);
+            }
+        };   
+   
         let mut last = String::new();
         let clipboard = Clipboard::new().unwrap();
         
         loop {
+            // check if clipboard changed OR if named pipe has input...
+            // that would figure everything
+            // or... what if there were multiple threads all doing this
+
+            // main communicator thread - listens to input from server & pings it occassionally
+            // one thread that listens on input from clipboard -> if detected, send to main to send to server
+        
             if let Ok(curr) = clipboard.load_wait(
                 clipboard.getter.atoms.primary,
                 clipboard.getter.atoms.utf8_string,
                 clipboard.getter.atoms.property,
             ) {
                 let curr = String::from_utf8_lossy(&curr);
-                let curr = curr.trim_matches('\u{0}').trim();          
+                let curr = curr.trim_matches('\u{0}').trim();
 
                 // dies when the user tries to copy paste something and the server is dead
-                // fine for now
                 if !curr.is_empty() && last != curr {
                     last = curr.to_owned();
                     match post_clipboard(&last.to_owned(), &opt, &client) {
                         Ok(_) => (),
                         Err(e) => {
                             eprintln!("{}", e);
-                            die()
+                            std::process::exit(-1);
                         }
                     };
                 }
-            }        
+            }
+
+            // read from named pipe & paste into clipboard
+            // have either the server automatically send changes to the pipe
+            // OR have the user have set up a script that grabs from the pipe and pastes into it
         }
     } else {
-        println!("Daemon is already running");
-        std::process::exit(-1)
+        eprintln!("Daemon is already running");
+        std::process::exit(-1);
     }
-}
-
-fn die() {
-
-    let mut curr_dir = std::env::current_exe().unwrap();
-
-    curr_dir.pop();
-    std::env::set_current_dir(&curr_dir).expect("Unable to change to new directory.");
-    
-    std::fs::remove_file("daemon.pid").expect("Unable to remove file.");
-    std::fs::remove_file("arguments.json").expect("Unable to remove file.");
-    std::process::exit(-1)
 }
 
 fn sanitize_input(opt: &mut Args) {
@@ -166,7 +115,6 @@ fn sanitize_input(opt: &mut Args) {
 }
 
 fn build_url(opt: &Args, url_path: &String) -> String {
-
     let mut url_string = String::from(&opt.url);
     url_string.push(':');
     url_string.push_str(&opt.port);
@@ -174,17 +122,18 @@ fn build_url(opt: &Args, url_path: &String) -> String {
     url_string.push_str(&opt.hostname);
 
     url_string
-    
 }
 
 fn kill_clipboard(opt: &Args) -> Result<(), reqwest::Error> {
-
     let mut map = HashMap::new();
     map.insert("passphrase", &opt.passphrase);
 
-    let url = build_url(opt, &"/api/kill_clipboard/".to_string());    
+    let url = build_url(opt, &"/api/kill_clipboard/".to_string());
     
-    reqwest::blocking::Client::new().post(url).json(&map).send()?;
+    reqwest::blocking::Client::new()
+        .post(url)
+        .json(&map)
+        .send()?;
     Ok(())
 }
 
@@ -195,23 +144,22 @@ fn create_clipboard(opt: &Args, client: &reqwest::blocking::Client) -> Result<()
 
     let url = build_url(opt, &"/api/create_clipboard/".to_string());
 
-    client.post(url).json(&map).send()?;    
-    
+    client.post(url).json(&map).send()?;
+
     Ok(())
 }
 
 fn post_clipboard(
-    contents: &String,    
+    contents: &String,
     opt: &Args,
     client: &reqwest::blocking::Client,
 ) -> Result<(), reqwest::Error> {
-    
     let mut map = HashMap::new();
     map.insert("contents", contents);
-    map.insert("passphrase", &opt.passphrase);   
-    
+    map.insert("passphrase", &opt.passphrase);
+
     let url = build_url(opt, &"/api/set_clipboard/".to_string());
-    
+
     client.post(url).json(&map).send()?;
 
     Ok(())
