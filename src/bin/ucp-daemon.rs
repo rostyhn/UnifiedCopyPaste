490aac8c8f5020793bc393d7af183f93c5c98d51
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2022-06-20 20:03:04 -0700

    Switched to websocket + multi-threading

--- src/bin/ucp-daemon.rs
+++ src/bin/ucp-daemon.rs
@@ -1,37 +1,42 @@
-extern crate daemonize;
-extern crate x11_clipboard;
-
-//use std::fs::File;
-//use daemonize::Daemonize;
-use x11_clipboard::Clipboard;
-
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::thread;
+
+use x11_clipboard::Clipboard;
 use structopt::StructOpt;
 use single_instance::SingleInstance;
-
 use serde::{Deserialize, Serialize};
+
+
+use websocket::client::ClientBuilder;
+use websocket::{OwnedMessage, Message};
 
 #[derive(Clone, Debug, StructOpt, Serialize, Deserialize)]
 struct Args {
-    #[structopt(short, long, default_value = "http://localhost")]
-    url: String,
+    // domain name only
+    #[structopt(short, long, default_value = "localhost")]
+    domain: String,
     #[structopt(short, long, default_value = "8000")]
     port: String,
+    // identifies the current daemon
     #[structopt(short, long, default_value = "")]
     hostname: String,
     #[structopt(long, default_value = "")]
-    passphrase: String
+    passphrase: String,
+    #[structopt(long, default_value = "")]
+    url: String,
+    #[structopt(short, long)]
+    ssl_enabled: bool
 }
 
 fn main() {
 
     let my_instance = SingleInstance::new("ucp-daemon").unwrap();
-
+    
     if my_instance.is_single() {
-
         let mut opt = Args::from_args();
         sanitize_input(&mut opt);
-    
+        
         if opt.hostname.trim().is_empty() {
             let hostname = hostname::get().expect("Failed to retrieve hostname.");
             opt.hostname = hostname.into_string().unwrap();
@@ -58,41 +63,120 @@ fn main() {
             }
         };   
    
-        let mut last = String::new();
-        let clipboard = Clipboard::new().unwrap();
         
         loop {
-            // check if clipboard changed OR if named pipe has input...
-            // that would figure everything
-            // or... what if there were multiple threads all doing this
+            let ws_str = build_ws_str(&opt);
 
-            // main communicator thread - listens to input from server & pings it occassionally
-            // one thread that listens on input from clipboard -> if detected, send to main to send to server
-        
-            if let Ok(curr) = clipboard.load_wait(
-                clipboard.getter.atoms.primary,
-                clipboard.getter.atoms.utf8_string,
-                clipboard.getter.atoms.property,
-            ) {
-                let curr = String::from_utf8_lossy(&curr);
-                let curr = curr.trim_matches('\u{0}').trim();
+            // this can be split into two!
+            let client = ClientBuilder::new(&ws_str)
+                .unwrap()
+                .connect_insecure()
+                .unwrap();
 
-                // dies when the user tries to copy paste something and the server is dead
-                if !curr.is_empty() && last != curr {
-                    last = curr.to_owned();
-                    match post_clipboard(&last.to_owned(), &opt, &client) {
+            let (mut receiver, mut sender) = client.split().unwrap();
+            
+            let (tx, rx) = mpsc::channel();
+
+            // reciever's channel to send back messages
+            let tx_1 = tx.clone();
+
+            // clipboard listener thread
+            thread::spawn(move || {
+                let mut last = String::new();
+                let clipboard = Clipboard::new().unwrap();
+                loop {
+                    if let Ok(curr) = clipboard.load_wait(
+                        clipboard.getter.atoms.primary,
+                        clipboard.getter.atoms.utf8_string,
+                        clipboard.getter.atoms.property,
+                    ) {
+                        let curr = String::from_utf8_lossy(&curr);
+                        let curr = curr.trim_matches('\u{0}').trim();
+
+                        if !curr.is_empty() && last != curr {
+                            last = curr.to_owned();
+                            match tx.send(Message::text(last.to_owned())) {
+                                Ok(_) => (),
+                                Err(e) => {
+                                    eprintln!("Error: {}", e);
+                                    std::process::exit(-1)
+                                }
+                            };
+                        }
+                    }
+                }
+            });
+
+            // websocket sender thread
+            thread::spawn(move || {
+                loop {
+                    let msg = match rx.recv() {
+                        Ok(m) => m,
+                        Err(e) => {
+                            eprintln!("Error: {}", e);
+                            std::process::exit(-1)
+                        }
+                    };
+                    
+                    let res = sender.send_message(&msg);
+                    match res {
                         Ok(_) => (),
                         Err(e) => {
-                            eprintln!("{}", e);
-                            std::process::exit(-1);
+                            eprintln!("Error: {}", e);
+                            std::process::exit(-1)
+                        }
+                    }
+                }
+            });
+
+            
+            // server listener thread - main
+            loop {
+
+                
+                for message in receiver.incoming_messages() {
+                    let msg = match message {
+                        Ok(m) => m,
+                        Err(e) => {
+                            eprintln!("Error: {}. Server stopped responding.", e);
+                            std::process::exit(-1)
+                        }
+                    };
+
+                    match msg {
+                        OwnedMessage::Ping(d) => {
+                            match tx_1.send(Message::pong(d)) {
+                                Ok(()) => (),
+                                Err(e) => {
+                                    eprintln!("Error: {}.", e);
+                                    std::process::exit(-1)
+                                }                                    
+                            };
+                        },
+                        OwnedMessage::Pong(d) => {
+                            match tx_1.send(Message::ping(d)) {
+                                Ok(()) => (),
+                                Err(e) => {
+                                    eprintln!("Error: {}.", e);
+                                    std::process::exit(-1)
+                                }                                    
+                            };
+                        },
+                        OwnedMessage::Close(_) => {
+                            let _ = tx_1.send(Message::close());
+                            
+                        },                                                    
+                        OwnedMessage::Text(d) => {
+                            // switch contents here
+                            dbg!("{}", d);
+                        },
+                        _ => {
+                            eprintln!("Message type not supported.");
+                            std::process::exit(-1)
                         }
                     };
                 }
-            }
-
-            // read from named pipe & paste into clipboard
-            // have either the server automatically send changes to the pipe
-            // OR have the user have set up a script that grabs from the pipe and pastes into it
+            }            
         }
     } else {
         eprintln!("Daemon is already running");
@@ -100,18 +184,9 @@ fn main() {
     }
 }
 
-fn sanitize_input(opt: &mut Args) {
-    if !opt.url.starts_with("http://") && !opt.url.starts_with("https://") {
-        opt.url.insert_str(0, "http://");
-    }
-
-    if opt.url.ends_with(':') {
-        opt.url.pop();
-    }
-
-    if opt.port.starts_with(':') {
-        opt.port.remove(0);
-    }
+fn sanitize_input(opt: &mut Args) {        
+    opt.url.insert_str(0, &get_http_str(opt.ssl_enabled));
+    opt.url.push_str(&opt.domain);         
 }
 
 fn build_url(opt: &Args, url_path: &String) -> String {
@@ -124,6 +199,35 @@ fn build_url(opt: &Args, url_path: &String) -> String {
     url_string
 }
 
+fn get_http_str(ssl_enabled: bool) -> String {
+    if ssl_enabled {
+       "https://".to_string() 
+    } else {
+        "http://".to_string()
+    }   
+}
+
+fn get_ws_str(ssl_enabled: bool) -> String {
+    if ssl_enabled {
+        "wss://".to_string()
+    } else {
+        "ws://".to_string()
+    }
+}
+
+fn build_ws_str(opt: &Args) -> String {
+    let mut ws_str = String::from(&opt.domain);
+    ws_str.insert_str(0, &get_ws_str(opt.ssl_enabled));
+    ws_str.push(':');
+    ws_str.push_str(&opt.port);
+    ws_str.push_str("/api/clipboard_websocket/");
+    ws_str.push_str(&opt.hostname);
+    
+    ws_str
+}
+
+/* All HTTP requests */
+    
 fn kill_clipboard(opt: &Args) -> Result<(), reqwest::Error> {
     let mut map = HashMap::new();
     map.insert("passphrase", &opt.passphrase);
@@ -149,7 +253,8 @@ fn create_clipboard(opt: &Args, client: &reqwest::blocking::Client) -> Result<()
     Ok(())
 }
 
-fn post_clipboard(
+#[deprecated]
+fn set_clipboard(
     contents: &String,
     opt: &Args,
     client: &reqwest::blocking::Client,
